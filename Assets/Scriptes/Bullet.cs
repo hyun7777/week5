da304f545602c11230d9bb8736b0a7440789b7b7
hyun <louisnjack@gmail.com>
2025-11-02 20:40:15 +0900

    VerticalShooting

---
+++ Assets/Scriptes/Bullet.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int dmg;
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "BorderBullet")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
